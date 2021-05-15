ec9924ef91d12fed7921cbbb5214f901aa9cec2b
Camisao.Lucas <lcscms326@gmail.com>
2021-05-15 17:21:13 -0300

    Pagina mostrando os filmes e atualizando conforme pesquisa, ainda não estilizada

--- BuscaFilmes/script.js
+++ BuscaFilmes/script.js
@@ -3,8 +3,31 @@ $('.btn').click(function(event){
     let resposta = $('.resposta').val().split(" ")
     let conversao = resposta.join(`+`)
     $.ajax({
-        "url" : `http://www.omdbapi.com/?apikey=708a483d&S=${conversao}&plot=full`,
-        "success" : ()=>{
+        //requesição de todos os filmes de acordo com a pesquisa.
+        "url" : `http://www.omdbapi.com/?apikey=708a483d&S=${conversao}`,
+        "success" : (req)=>{
+            console.log(req)
+            let recebe = req.Search
+            
+        //passa por dentro da array de filmes recebida.
+            for(let i = 0; i<recebe.length; i++){
+        //verifica se o o typo realmente é um filme
+            if(recebe[i].Type == 'movie'){   
+
+                let filmes = new Filmes(recebe[i]);
+
+        $.ajax({
+            //requisiçao de todas(FULL) as informações de cada filme.
+            "url" : `http://www.omdbapi.com/?apikey=708a483d&t=${recebe[i].Title}&plot=full`,
+            "success" : (req)=>{
+                filmes.pegaPlot(req.Plot)
+            },
+            'error':function(erro){
+            }
+            });
+                
+                filmes.mostrarFilmes()
+            }}
             
         },
         'error':function(erro){
@@ -12,13 +35,36 @@ $('.btn').click(function(event){
       });
 });
 
+
 class Filmes{
-    constructor(poster, titulo, ano){
-        this.poster = poster,
-        this.titulo = titulo,
-        this.ano = ano
+    constructor(results){
+
+        this.img = results.Poster;
+        this.nome = results.Title;
+        this.descricao;
+        this.ano = results.Year;
+        this.lancamento = results.Released;
+        this.duracao = results.Runtime;
+        this.genero = results.Genre;
+        this.diretor = results.Director;
     }
 
-    
+    mostrarFilmes(){
+        $(`#posicao-filmes`).html(`<div id="posicao-filmes"></div>`)
 
+        setTimeout(() => {
+            
+             $(`#posicao-filmes`).append(`
+            <h1>${this.nome}</h1>
+            <img src = ${this.img}>
+            <p>${this.descricao}</p>
+            <p>${this.genero}</p>`) 
+          
+        }, 500);              
+    }
+
+    pegaPlot(requesicao){
+        this.descricao = requesicao
+    } 
 }
+

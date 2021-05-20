ec53d268a1702a34fb95ca3c116c7bd71ff42f29
Daniel-Roch <daniel_roch4@hotmail.com>
2021-05-20 01:13:23 -0300

    melhorando site, colocando a parte de validação de e-mail e resposta do recuperação de senha.

--- Login e Recuperar senha/mostra.js
+++ Login e Recuperar senha/mostra.js
@@ -1,31 +1,24 @@
-//criar array com 12filmes 
-let arrId = ['tt0417741', 'tt1457767', 'tt2305051', 'tt5814060', 'tt0903624',
-    'tt0113497', 'tt0107290', 'tt0114709', 'tt0094862', 'tt2397535', 'tt4154796', 'tt0081505']
+//valida e-mail para recuperação de senha
+$('#enviar-recSenha').on('click', (event)=>{
+    event.preventDefault()
+    const email = $('#exampleInputEmail1').val()
 
-$(document).ready(function () {
-           //percorrer o array
-for (let i = 0; i < arrId.length; i++) {
-    //requisicao 
-    $.ajax({
-        url: 'http://www.omdbapi.com/',
-        //organizacao 
-        'data': {
-            'i': arrId[i],
-            'apikey': '69fafd31'
-
-    
-        },
-        success: function (resultado) {
-           // console.log(resultado.Actors)
-             //exibir img 
-            document.querySelector('#principal').innerHTML += `<img src="${resultado.Poster}"> `
-    
-    
-        }
-    
-    })
-}
-    })
+    if(email.indexOf('@') >= 0){
+        $('.erro-rec').css('display', 'none')
+        $('#formulario-deRecS').css('display', 'none')    
+        $('#respo-recS').html(`
+        <div class="resp-recup">
+        <h2>Envio realizado com <u>sucesso!</u></h2>
+        <img class="img-respo" src="./img login/9ce5d338b51fbd7b15cf148e70b12377.gif">
+        <h3>Favor checar sua caixa de entrada.</h3>
+        <a class="nav-link text-light cadastro" href="./login.html">voltar</a>
+        </div>`)
+    }else{
+        $('.erro-rec').html(`
+        <p>E-mail inválido.</p>
+        `)
+    }
+})
  
 
 
